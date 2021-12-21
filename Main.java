2d64239ffacc167fa3c00833f1e31a7723ce8078
WaniyaMustafa <91070350+WaniyaMustafa@users.noreply.github.com>
2021-12-21 19:45:48 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author waniya_mustafa
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        mainFrame obj = new mainFrame();
+        obj.setVisible(true);
+        userLoginFrame obj2 = new userLoginFrame();
+        obj2.setVisible(true);
+        adminLoginFrame obj4 = new adminLoginFrame();
+        obj4.setVisible(true);
+       ChangePassword obj3 = new ChangePassword();
+        obj3.setVisible(true);
+    transferMoneyFrame obj5 = new transferMoneyFrame();
+        obj5.setVisible(true);
+    userSignUpFrame obj6 = new userSignUpFrame();
+        obj6.setVisible(true);
+    MainMenuFrame obj7 = new MainMenuFrame();
+        obj7.setVisible(true);
+   
+    }
+
+}
